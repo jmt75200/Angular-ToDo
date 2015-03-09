e1e19ae8eb78d31c342d1015916e10b00c84673e
jmt75200 <joelle@pcgamerzhawaii.com>
2015-03-09 12:01:38 -1000

    adding working server

---
+++ server/controllers/api.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function (req, res){
+  res.send("Jello API");
+});
+
+//add todo
+
+//delete todo
+
+//complete todo
+
+//uncomplete todo
+
+
+module.exports = router;
